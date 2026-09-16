23fb3681f6f0516347f1bd73df25567ae64933e9
kmos <kmos@commonhaus.dev>
2026-09-16 18:46:38 +0200

    remove copyrights

--- debezium-jbang-rag/src/main/jbang/main/RagCLI.java
+++ debezium-jbang-rag/src/main/jbang/main/RagCLI.java
@@ -1,8 +1,3 @@
-/*
- * Copyright Debezium Authors.
- *
- * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
- */
 
 //JAVA 21+
 //REPOS central=https://repo1.maven.org/maven2,apache-snapshot=https://repository.apache.org/content/groups/snapshots/
